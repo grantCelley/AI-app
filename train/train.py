bd52ea1f1727a43d583723a539e924502d613cba
Grant Celley <grant.celley@gmail.com>
2020-10-21 00:28:06 -0400

    Got the annotations right. Some were misspelled

--- train/train.py
+++ train/train.py
@@ -7,6 +7,8 @@ from detectron2.config import get_cfg
 from detectron2.data.datasets import register_pascal_voc
 from detectron2.utils import logger
 
-classNames=['Presser Foot', 'Zipper Foot','Sliding Button Foot', 'Blind Hem Foot', 'Bobon', 'Seem Ripper', 'Seem Ripper Lid', 'Screw Driver']
+classNames=['Presser Foot', 'Zipper Foot','Sliding Button Foot', 'Blind Hem Foot', 'Bobon', 'Seam Ripper', 'Seam Ripper Lid', 'Screw Driver']
 
 register_pascal_voc("Janome 2212", "../data",'train', 2020, class_names=classNames)
+
+print(DatasetCatalog.get('Janome 2212'))
