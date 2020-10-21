8bd4052c44b7a901ebd2caa7e0f142dada693fc8
Grant Celley <grant.celley@gmail.com>
2020-10-21 00:07:17 -0400

    I fixed a typo in the last one I am using detectron2 not detectron

--- train/train.py
+++ train/train.py
@@ -5,7 +5,7 @@ import detectron2
 from detectron2.data import MetadataCatalog, DatasetCatalog
 from detectron2.config import get_cfg
 from detectron2.data.datasets import register_pascal_voc
-from detectron.utils import logger
+from detectron2.utils import logger
 
 classNames=['Presser Foot', 'Zipper Foot','Sliding Button Foot', 'Blind Hem Foot', 'Bobon', 'Seem Ripper', 'Seem Ripper Lid', 'Screw Driver']
 
