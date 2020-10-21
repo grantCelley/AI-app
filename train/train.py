6fde811c407c2a3a6724a01af38be1c00ee7e66a
Grant Celley <grant.celley@gmail.com>
2020-10-21 00:05:57 -0400

    changed the train split

--- train/train.py
+++ train/train.py
@@ -2,8 +2,11 @@ import torch
 import torchvision
 
 import detectron2
+from detectron2.data import MetadataCatalog, DatasetCatalog
+from detectron2.config import get_cfg
 from detectron2.data.datasets import register_pascal_voc
+from detectron.utils import logger
 
 classNames=['Presser Foot', 'Zipper Foot','Sliding Button Foot', 'Blind Hem Foot', 'Bobon', 'Seem Ripper', 'Seem Ripper Lid', 'Screw Driver']
 
-register_pascal_voc("Janome 2212", "../data",'test', 2020, class_names=classNames)
+register_pascal_voc("Janome 2212", "../data",'train', 2020, class_names=classNames)
