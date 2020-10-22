afa571a67d6389b45dbf3da87224c448d3edc74b
Grant Celley <grant.celley@gmail.com>
2020-10-22 17:42:26 -0400

    Got the training to work from what I can tell. It only works on cuda which is not on my machine. Will make a docker file. I had to also rename my files with the extension of .jpg instead of .JPG because of labelIMG

--- train/train.py
+++ train/train.py
@@ -1,7 +1,29 @@
 import torch
 import torchvision
+import os
 from detectron2.data.datasets import register_pascal_voc
+from detectron2.config import get_cfg
+from detectron2.engine import DefaultTrainer
+from detectron2 import model_zoo
 
-classNames=['Presser Foot', 'Zipper Foot','Sliding Button Foot', 'Blind Hem Foot', 'Bobon', 'Seem Ripper', 'Seem Ripper Lid', 'Screw Driver']
+classNames=['Presser Foot', 'Zipper Foot','Sliding Button Foot', 'Blind Hem Foot', 'Bobon', 'Seam Ripper', 'Seam Ripper Lid', 'Screw Driver']
 
 register_pascal_voc("Janome 2212", "../data",'train', 2020, class_names=classNames)
+
+cfg = get_cfg()
+cfg.merge_from_file(model_zoo.get_config_file("COCO-InstanceSegmentation/mask_rcnn_R_50_FPN_3x.yaml"))
+cfg.DATASETS.TRAIN = ("Janome 2212",)
+cfg.DATASETS.TEST = ()
+cfg.DATALOADER.NUM_WORKERS = 4
+cfg.MODEL.WEIGHTS = model_zoo.get_checkpoint_url("COCO-InstanceSegmentation/mask_rcnn_R_50_FPN_3x.yaml")  # Let training initialize from model zoo
+cfg.SOLVER.IMS_PER_BATCH = 1
+cfg.SOLVER.BASE_LR = 0.00025  # pick a good LR
+cfg.SOLVER.MAX_ITER = 300    # 300 iterations seems good enough for this toy dataset; you will need to train longer for a practical dataset
+cfg.MODEL.ROI_HEADS.BATCH_SIZE_PER_IMAGE = 8   # faster, and good enough for this toy dataset (default: 512)
+cfg.MODEL.ROI_HEADS.NUM_CLASSES = 1  # only has one class (ballon). (see https://detectron2.readthedocs.io/tutorials/datasets.html#update-the-config-for-new-datasets)
+
+
+os.makedirs(cfg.OUTPUT_DIR, exist_ok=True)
+trainer = DefaultTrainer(cfg) 
+trainer.resume_or_load(resume=False)
+trainer.train()
