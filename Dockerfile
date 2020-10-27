2c206f29f383c4e658380794a642a3c68e7fb985
Grant Celley <grant.celley@gmail.com>
2020-10-27 23:03:59 +0000

    So I need to reduce the image size in tensors

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,7 @@
 FROM tensorflow/tensorflow:2.3.1-gpu
 
+ARG DEBIAN_FRONTEND=noninteractive
+
 RUN apt-get update && apt-get install -y \
     git \
     gpg-agent \
@@ -11,8 +13,15 @@ RUN apt-get update && apt-get install -y \
     wget
 
 
-RUN git clone --depth 1 https://github.com/tensorflow/models && \
-  protoc models/research/object_detection/protos/*.proto --pyhton_out=models/research && \
-  cp models/research/object_detection/packages/tf2/setup.py models/research && \
-  python3 -m pip install -q models/research
+RUN git clone --depth 1 https://github.com/tensorflow/models
+
+WORKDIR models/research
+RUN protoc object_detection/protos/*.proto --python_out=. && \
+  cp object_detection/packages/tf2/setup.py . && \
+  python3 -m pip install -q . && \
+  wget http://download.tensorflow.org/models/object_detection/tf2/20200711/ssd_mobilenet_v2_fpnlite_640x640_coco17_tpu-8.tar.gz && \
+  tar -xf ssd_mobilenet_v2_fpnlite_640x640_coco17_tpu-8.tar.gz && \
+  mv ssd_mobilenet_v2_fpnlite_640x640_coco17_tpu-8/checkpoint object_detection/test_data/
+
+WORKDIR /app
 
--- train/train.py
+++ train/train.py
@@ -5,14 +5,16 @@ from lxml import etree
 import tensorflow as tf
 import numpy as np
 
-from object_detection.utils import dataset_util
+from object_detection.utils import dataset_util, config_util
+from object_detection.builders import model_builder
 
 
 dataset = '../data/ImageSets/Main'
 annotations_dir = '../data/Annotations'
 ImageDir = '../data/JPEGImages'
-classes =  ['Background','Presser Foot', 'Zipper Foot', 'Sliding Button Foot', 'Blind Hem Foot', 'Bobon', 'Seam Ripper', 'Seam Ripper Lid']
-num_classes = 8
+classes =  ['Background','Presser Foot', 'Zipper Foot', 'Sliding Button Foot', 'Blind Hem Foot', 'Bobon','Screw Driver', 'Seam Ripper', 'Seam Ripper Lid']
+num_classes = len(classes)
+print(num_classes)
 label_id_offset = 1
 train_image_np = []
 train_image_tensors = []
@@ -69,5 +71,34 @@ for i in image_list:
 		#f_width = f_xmax - f_x
 
 		bx_tensors.append(tf.convert_to_tensor([[f_x, f_y, f_xmax, f_ymax]],dtype=np.float32))
-		zero_indexed_classes = tf.convert_to_tensor(np.array([class_ind], dtype=np.int) - label_id_offset)
+		zero_indexed_classes = tf.convert_to_tensor(class_ind - label_id_offset, dtype=tf.int32)
 		classes_one_hot_tensors.append(tf.one_hot(zero_indexed_classes, num_classes))
+
+tf.keras.backend.clear_session()
+pipline_config = '/models/research/object_detection/configs/tf2/ssd_mobilenet_v2_fpnlite_640x640_coco17_tpu-8.config'
+cheackpoint_path = './ckpt-0'
+
+configs = config_util.get_configs_from_pipeline_file(pipeline_config)
+model_config = configs['model']
+model_config.ssd.num_classes = num_classes
+model_config.ssd.freeze_batchnorm = True
+detection_model = model_builder.build(
+	model_config=model_config, is_training=True)
+
+fake_box_predictor = tf.compat.v2.train.Checkpoint(
+    _base_tower_layers_for_heads=detection_model._box_predictor._base_tower_layers_for_heads,
+     _box_prediction_head=detection_model._box_predictor._box_prediction_head,
+    )
+
+fake_model = tf.compat.v2.train.Checkpoint(
+          _feature_extractor=detection_model._feature_extractor,
+          _box_predictor=fake_box_predictor)
+ckpt = tf.compat.v2.train.Checkpoint(model=fake_model)
+ckpt.restore(checkpoint_path).expect_partial()
+
+image, shapes = detection_model.preprocess(tf.zeros([1, 640, 640, 3]))
+prediction_dict = detection_model.predict(image, shapes)
+_ = detection_model.postprocess(prediction_dict, shapes)
+print('Weights restored!')
+
+tf.keras.backend.set_learning_phase(True)
